856dc5584f16ec40610f4dabf25ce8912427b1ea
abo3adel <abo3adel35@gmail.com>
2025-10-03 23:51:48 +0300

    feat(urls): add model

---
+++ database/factories/UrlFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Url>
+ */
+class UrlFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => fn () => \App\Models\User::factory()->create(),
+            'url' => fake()->url,
+            'short_code' => fake()->unique()->regexify('[a-zA-Z0-9]{6}'),
+            'clicks' => 0,
+        ];
+    }
+}
