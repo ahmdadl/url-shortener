cf390bdb7892bcdb52a334d9733a261215c08034
abo3adel <abo3adel35@gmail.com>
2025-10-03 23:52:55 +0300

    feat(urls): add model

--- database/migrations/2025_10_03_112833_create_urls_table.php
+++ database/migrations/2025_10_03_112833_create_urls_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('urls', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id')->constrained();
-            $table->string('url');
+            $table->string('url')->index();
             $table->string('short_code')->unique();
             $table->integer('clicks')->default(0);
             $table->timestamps();
